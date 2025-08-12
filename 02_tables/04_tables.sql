30c548c41548537891d119cc269e813ac51360e5
Lokthok <lokthok.git@mailbox.org>
2025-08-12 11:01:46 +0200

    04_tables PWs in Klartext

--- 02_tables/04_tables.sql
+++ 02_tables/04_tables.sql
@@ -21,18 +21,19 @@ CREATE TABLE IF NOT EXISTS kunde
     k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45) NOT NULL DEFAULT "TBA",
     k_alter TINYINT,
-    k_mail VARCHAR(100) UNIQUE
+    k_mail VARCHAR(100) UNIQUE,
+    k_pw VARCHAR(100) 
 );
 
 -- Struktur anzeigen
 DESCRIBE kunde;
 
 -- Datensätze/Zeilen
-INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT, "Meier", 28, "meier@web.de");
-INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT, "Hussein", 32, "hussein@web.de");
-INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT, "Li", 26, "li@web.de");
-INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT, "Li", 40, "li-ber@web.de");
-INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT, "Li", 58, "li-sa@web.de");
+INSERT INTO kunde(k_id, k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT, "Meier", 28, "meier@web.de", "katsching");
+INSERT INTO kunde(k_id, k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT, "Hussein", 32, "hussein@web.de", "zeuch");
+INSERT INTO kunde(k_id, k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT, "Li", 26, "li@web.de", "niemals123");
+INSERT INTO kunde(k_id, k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT, "Li", 40, "li-ber@web.de", "zack129812zack");
+INSERT INTO kunde(k_id, k_name, k_alter, k_mail, k_pw) VALUES(DEFAULT, "Li", 58, "li-sa@web.de", "sachen machen");
 
 INSERT INTO kunde(k_id, k_name, k_alter, k_mail) VALUES(DEFAULT, NULL, NULL, NULL);
 
